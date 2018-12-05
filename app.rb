93b034efdfdb54ab4b2091fcbcc03bd02c08d96c
TAKExHERO <yamaguchi.takehiro0129@gmail.com>
2018-12-06 01:09:11 +0900

    six-commit

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 equire 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   'hello world!!'
 end
 
 get '/callback' do
-  if params["hub.verify_token"] != 'hogehoge' 
+  if params["hub.verify_token"] != 'hogehoge'
     return 'Error, wrong validation token'
   end
   params["hub.challenge"]
+end
+
+post '/callback' do
+  request_body = JSON.parse(request.body.read)
+  puts request_body
+  status 201
+  body ''
 end
