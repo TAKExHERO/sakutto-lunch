dd5819052528b0713589676908bb546df006e617
TAKExHERO <yamaguchi.takehiro0129@gmail.com>
2018-12-06 01:13:54 +0900

    six-commit

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
-equire 'sinatra'
+require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'rest_client'
 
 get '/' do
   'hello world!!'
@@ -14,8 +15,19 @@ get '/callback' do
 end
 
 post '/callback' do
-  request_body = JSON.parse(request.body.read)
-  puts request_body
+  hash = JSON.parse(request.body.read)
+  message = hash["entry"][0]["messaging"][0] #entryの0個目のmessagingの0個目
+  sender = message["sender"]["id"] #上記で取得したmessage変数の中のsenderのid
+  text = message["message"]["text"]#上記で取得したmessage変数の中のmessageのtest
+  endpoint = "https://graph.facebook.com/v2.6/me/messages?access_token=" + "EAAfqufUBZC5gBAJ7NZAC0uhXhwXwY56WaFNqR9ea4z2RDNoPxZAA6mq8vzoVbWqaGSKIgMH8EUnpMpNbKlg1GFOEhzPZBZAZBEZCB0dq7R5bjVNKpSAULZAJkZAvzANksCB5Klyp1bZACHm5nnlpLclfj8GX5Ry1jYg4ODAcbiznlFDlT7ZBAnu06ZBd"
+  content = {
+    recipient: {id: sender},
+    message: {text: text}
+  }
+  request_body = content.to_json
+
+  #オウム返しの返信をPOSTする（返す）
+  RestClient.post endpoint, request_body, content_type: :json, accept: :json
   status 201
   body ''
 end
